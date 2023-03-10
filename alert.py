87ffbc8d817cd9c69145342e3e0dea75f10535b5
cololaborde <cololaborde@gmail.com>
2023-03-10 20:42:42 -0300

    lynt

--- alert.py
+++ alert.py
@@ -1,13 +1,15 @@
-import requests
-import threading
-import time
-import os
-from sys import exit as terminate
-from load_environ import load_environ
 
 """ Para obtener el id del chat, se tiene que enviar un mensaje al chat del bot y acceder a 
     https://api.telegram.org/bot{{api_token}}/getUpdates
 """
+
+import os
+import time
+import threading
+from sys import exit as terminate
+import requests
+from load_environ import load_environ
+
 
 def send_to_telegram(message):
     """ send telegram message """
@@ -23,24 +25,27 @@ def send_to_telegram(message):
 
 
 def get_global_ip(retries, response):
+    """ get global ip retrying retries times and save data in response array """
     if retries > 0:
         try:
             response[0] = requests.get('http://ifconfig.me', verify=False, timeout=10)
             return response
-        except:
+        except Exception:
             time.sleep(5)
             get_global_ip(retries-1, response)
+            return None
     else:
         return [None]
 
-response = [None]*1
-t = threading.Thread(daemon=True, target=get_global_ip, args=(5, response))
+resp = [None]*1
+t = threading.Thread(daemon=True, target=get_global_ip, args=(5, resp))
 t.start()
 t.join()
 
-if not response[0]:
+if not resp[0]:
     terminate()
 
-global_ip = response[0].content.decode()
-text = f"Nuevo encendido desde: {global_ip} en Windows \n\n Mas información en: https://www.infobyip.com/ip-{global_ip}.html"
+global_ip = resp[0].content.decode()
+text = f"Nuevo encendido desde: {global_ip} en Windows \n\n \
+Mas información en: https://www.infobyip.com/ip-{global_ip}.html"
 send_to_telegram(text)
