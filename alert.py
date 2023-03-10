27f600bf41a4339c96e797dd8e46278e91bf9499
cololaborde <cololaborde@gmail.com>
2023-03-10 17:38:31 -0300

    add get global ip function

--- alert.py
+++ alert.py
@@ -11,3 +11,14 @@ def send_to_telegram(message):
         requests.post(api_url, json={'chat_id': chat_id, 'text': message}, verify=False, timeout=10)
     except Exception as exception:
         print(exception)
+
+def get_global_ip(retries, response):
+    if retries > 0:
+        try:
+            response[0] = requests.get('http://ifconfig.me', verify=False, timeout=10)
+            return response
+        except:
+            time.sleep(5)
+            get_global_ip(retries-1, response)
+    else:
+        return None
