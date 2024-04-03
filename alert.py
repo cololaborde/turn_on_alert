051f061cdfdfb1826dc277e52759bff5e8e5d86b
cololaborde <cololaborde@gmail.com>
2024-04-03 00:34:26 -0300

    adapt

--- alert.py
+++ alert.py
@@ -3,7 +3,6 @@
     https://api.telegram.org/bot{{api_token}}/getUpdates
 """
 
-import os
 import time
 import threading
 import platform
@@ -11,7 +10,8 @@ from sys import exit as terminate
 import cv2
 import requests
 from load_environ import load_environ
-
+import os
+from services.telegram_service import TelegramService
 
 RETRIES = 5
 TIME_OUT = 10
@@ -19,6 +19,7 @@ PHOTO_NAME = 'ptoa_rdk.png'
 
 
 def take_photo():
+    from os import remove
     """ try to take a photo using default cam """
     cap = cv2.VideoCapture(0)
 
@@ -32,30 +33,10 @@ def take_photo():
     
     cv2.imwrite(PHOTO_NAME, frame)
     photo = open(PHOTO_NAME, 'rb')
-    os.remove(PHOTO_NAME)
+    remove(PHOTO_NAME)
     cap.release()
     return photo
 
-
-def send_to_telegram(message, photo=None):
-    """ send telegram message """
-    load_environ()
-    api_token = os.environ.get("tlg_api_key")
-    chat_id = os.environ.get("chat_id")
-    api_base_url = f'https://api.telegram.org/bot{api_token}'
-    data = {'chat_id': chat_id, 'text': message}
-    try:
-        requests.post(f'{api_base_url}/sendMessage', data=data, verify=False, timeout=10)
-    except Exception as exception:
-        print(exception)
-
-    if not photo:
-        return
-    data = {'chat_id': chat_id}
-    try:
-        requests.post(f'{api_base_url}/sendPhoto', data=data, files={'photo': photo}, verify=False, timeout=10)
-    except Exception as exception:
-        print(exception)
 
 
 def get_global_ip(retries, response):
@@ -86,8 +67,16 @@ def create_thread():
 
 
 global_ip = create_thread()
-picture = take_photo()
+# picture = take_photo()
 text = f"Nuevo encendido desde: {global_ip} en {platform.system()} \n\n \
 Fecha y hora: {time.strftime('%d/%m/%Y %H:%M:%S')} \n\n \
 Mas información en: https://www.infobyip.com/ip-{global_ip}.html"
-send_to_telegram(text, picture)
+
+load_environ()
+tlg_service = TelegramService(
+    os.environ.get("tlg_api_key"),
+    os.environ.get("chat_id")
+)
+
+tlg_service.send_message(text)
+tlg_service.get_updates()
