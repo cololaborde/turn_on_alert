a70ab0f3a3ddd940eda40c2eadc6604579fd0aff
cololaborde <cololaborde@gmail.com>
2024-04-03 20:55:27 -0300

    thread return value on join iface

---
+++ utils/thread_with_return_value.py
@@ -0,0 +1,19 @@
+from threading import Thread
+
+
+class ThreadWithReturnValue(Thread):
+
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None, daemon=None):
+        Thread.__init__(self, group, target, name, args, kwargs, daemon=daemon)
+        self._return = None
+
+    def run(self):
+        print(self._target)
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                        **self._kwargs)
+
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
