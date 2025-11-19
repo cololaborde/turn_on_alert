ddfacc17aa85093ae53437f09f79efaa07f0e10f
cololaborde <cololaborde@gmail.com>
2025-11-18 22:10:14 -0300

    create os subclasses

---
+++ services/base_os_service.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from os import environ
+from utils.load_environ import load_environ
+
+class BaseOSService(ABC):
+
+    def __init__(self):
+        load_environ()
+
+    @abstractmethod
+    def get_screenshot(self): ...
+
+    @abstractmethod
+    def lock_screen(self): ...
+
+    @abstractmethod
+    def turn_off(self): ...
+
+    @abstractmethod
+    def mute_system(self): ...
+
+    @abstractmethod
+    def unmute_system(self): ...
+
+    def set_environ(self, key, value):
+        environ[key] = value
+
+    def get_environ(self, key):
+        return environ.get(key)
---
+++ services/linux_os_service.py
@@ -0,0 +1,62 @@
+import shutil
+from os import system as os_system
+from PIL import ImageGrab
+from services.base_os_service import BaseOSService
+
+
+class LinuxOSService(BaseOSService):
+
+    def get_screenshot(self):
+        path = "screenshot.png"
+
+        if shutil.which("grim"):
+            os_system(f"grim {path}")
+        elif shutil.which("gnome-screenshot"):
+            os_system(f"gnome-screenshot -f {path}")
+        elif shutil.which("scrot"):
+            os_system(f"scrot {path}")
+        elif shutil.which("maim"):
+            os_system(f"maim {path}")
+        else:
+            ImageGrab.grab().save(path, "PNG")
+
+        return open(path, "rb")
+
+    def lock_screen(self):
+        if shutil.which("i3lock"):
+            os_system("i3lock")
+        elif shutil.which("swaylock"):
+            os_system("swaylock")
+        elif shutil.which("gnome-screensaver-command"):
+            os_system("gnome-screensaver-command -l")
+        elif shutil.which("cinnamon-screensaver-command"):
+            os_system("cinnamon-screensaver-command -l")
+        elif shutil.which("mate-screensaver-command"):
+            os_system("mate-screensaver-command -l")
+        elif shutil.which("xflock4"):
+            os_system("xflock4")
+        elif shutil.which("qdbus"):
+            os_system("qdbus org.freedesktop.ScreenSaver /ScreenSaver Lock")
+        elif shutil.which("omarchy-lock-screen"):
+            os_system("omarchy-lock-screen")
+        else:
+            raise Exception("No lock screen available.")
+
+    def turn_off(self):
+        os_system("poweroff")
+
+    def mute_system(self):
+        if shutil.which("pactl"):
+            os_system("pactl set-sink-mute @DEFAULT_SINK@ 1")
+        elif shutil.which("amixer"):
+            os_system("amixer set Master mute")
+        else:
+            raise Exception("Cannot mute on this Linux.")
+
+    def unmute_system(self):
+        if shutil.which("pactl"):
+            os_system("pactl set-sink-mute @DEFAULT_SINK@ 0")
+        elif shutil.which("amixer"):
+            os_system("amixer set Master unmute")
+        else:
+            raise Exception("Cannot unmute on this Linux.")
