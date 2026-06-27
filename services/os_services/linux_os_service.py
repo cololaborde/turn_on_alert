b0c5ff1b12f2ac8564fae7c7226d6b4456ca4387
cololaborde <cololaborde@gmail.com>
2026-06-27 18:23:26 -0300

    fix wayland screenshot

--- services/os_services/linux_os_service.py
+++ services/os_services/linux_os_service.py
@@ -3,6 +3,9 @@ from os import system as os_system, environ as os_environ
 from PIL import ImageGrab
 from services.os_services.base_os_service import BaseOSService
 import subprocess
+import re
+import time
+from pathlib import Path
 
 LOCKERS = [
     ["loginctl", "lock-session"],
@@ -12,24 +15,53 @@ LOCKERS = [
     ["xflock4"],
 ]
 
+def _get_screen_path():
+    path = Path(os_environ.get("screenshots_path"))
 
+    files = list(path.glob("Screenshot-*"))
+
+    def get_num(f):
+        match = re.search(r"Screenshot-(\d+)", f.stem)
+        return int(match.group(1)) if match else -1
+
+    last_file_path = max(files, key=get_num)
+
+    return last_file_path
+
+def _get_wayland_screenshot():
+    cmd = [
+        "gdbus", "call",
+        "--session",
+        "--dest", "org.freedesktop.portal.Desktop",
+        "--object-path", "/org/freedesktop/portal/desktop",
+        "--method", "org.freedesktop.portal.Screenshot.Screenshot",
+        "",
+        "@a{sv} {}"
+    ]
+
+    subprocess.Popen(cmd, stdout=subprocess.PIPE, text=True)
+
+    return _get_screen_path()
 
 class LinuxOSService(BaseOSService):
 
     def get_screen_shot(self):
         path = "screenshot.png"
 
-        if shutil.which("grim"):
-            os_system(f"grim {path}")
-        elif shutil.which("gnome-screenshot"):
-            os_system(f"gnome-screenshot -f {path}")
-        elif shutil.which("scrot"):
-            os_system(f"scrot {path}")
-        elif shutil.which("maim"):
-            os_system(f"maim {path}")
+        session_type = os_environ.get("XDG_SESSION_TYPE", "").lower()
+        if session_type == "wayland":
+            path = _get_wayland_screenshot()
         else:
-            ImageGrab.grab().save(path, "PNG")
-
+            if shutil.which("grim"):
+                os_system(f"grim {path}")
+            elif shutil.which("gnome-screenshot"):
+                os_system(f"gnome-screenshot -f {path}")
+            elif shutil.which("scrot"):
+                os_system(f"scrot {path}")
+            elif shutil.which("maim"):
+                os_system(f"maim {path}")
+            else:
+                ImageGrab.grab().save(path, "PNG")
         return open(path, "rb")
 
     def lock_screen(self):
